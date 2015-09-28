29fddd2d14b1ed4cdf0a1087746e7608ed3ec1df
Jáchym Toušek <enumag@gmail.com>
2015-09-28 19:49:44 +0200

    Add PropertyAccessor configuration

--- src/PropertyAccess/DI/PropertyAccessExtension.php
+++ src/PropertyAccess/DI/PropertyAccessExtension.php
@@ -10,13 +10,33 @@ use Nette\DI\CompilerExtension;
 class PropertyAccessExtension extends CompilerExtension
 {
 
+	public $defaults = [
+		'magicCall' => false,
+		'throwExceptionOnInvalidIndex' => false,
+	];
+
 	public function loadConfiguration()
 	{
+		$this->validateConfig($this->defaults);
+
 		$builder = $this->getContainerBuilder();
+
+		$definition = $builder->addDefinition($this->prefix('propertyAccessorBuilder'));
+		$definition->setClass('Symfony\Component\PropertyAccess\PropertyAccessorBuilder');
+		if ($this->config['magicCall']) {
+			$definition->addSetup('enableMagicCall');
+		} else {
+			$definition->addSetup('disableMagicCall');
+		}
+		if ($this->config['throwExceptionOnInvalidIndex']) {
+			$definition->addSetup('enableExceptionOnInvalidIndex');
+		} else {
+			$definition->addSetup('disableExceptionOnInvalidIndex');
+		}
 
 		$builder->addDefinition($this->prefix('propertyAccessor'))
 			->setClass('Symfony\Component\PropertyAccess\PropertyAccessorInterface')
-			->setFactory('Symfony\Component\PropertyAccess\PropertyAccess::createPropertyAccessor');
+			->setFactory('@Symfony\Component\PropertyAccess\PropertyAccessorBuilder::getPropertyAccessor');
 	}
 
 }
