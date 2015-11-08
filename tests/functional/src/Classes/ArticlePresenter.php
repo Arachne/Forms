362fe5eb4a011029b5b7035460ceeb2b04c7fc61
Jáchym Toušek <enumag@gmail.com>
2015-11-08 19:44:41 +0100

    Add functional test

---
+++ tests/functional/src/Classes/ArticlePresenter.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Functional\Classes;
+
+use Arachne\Forms\Application\FormComponentFactory;
+use Nette\Application\UI\Presenter;
+use Symfony\Component\Form\FormFactoryInterface;
+
+/**
+ * @author Jáchym Toušek <enumag@gmail.com>
+ */
+class ArticlePresenter extends Presenter
+{
+
+	/**
+	 * @var FormComponentFactory
+	 * @inject
+	 */
+	public $formComponentFactory;
+
+	/**
+	 * @var FormFactoryInterface
+	 * @inject
+	 */
+	public $formFactory;
+
+	public function actionDefault()
+	{
+	}
+
+	protected function createComponentForm()
+	{
+		$builder = $this->formFactory->createBuilder('form', new Task());
+		$builder->add('text', 'text');
+		$builder->add('save', 'submit');
+
+		$component = $this->formComponentFactory->create($builder->getForm());
+		$component->onSuccess[] = function () {
+			$this->getTemplate()->state = 'success';
+		};
+		$component->onError[] = function () {
+			$this->getTemplate()->state = 'error';
+		};
+
+		return $component;
+	}
+
+	public function formatTemplateFiles()
+	{
+		$name = $this->getName();
+		$presenter = substr($name, strrpos(':' . $name, ':'));
+		return [ __DIR__ . "/../../templates/$presenter.$this->view.latte" ];
+	}
+
+}
---
+++ tests/functional/src/SimpleFormTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Functional;
+
+use Codeception\TestCase\Test;
+
+/**
+ * @author Jáchym Toušek <enumag@gmail.com>
+ */
+class SimpleFormTest extends Test
+{
+
+	public function testRendering()
+	{
+		$this->guy->amOnPage('/article/');
+		$this->guy->seeResponseCodeIs(200);
+		$this->guy->see(null, 'form');
+		$this->guy->see(null, 'input#form__token'); // CSRF protection
+		$this->guy->see(null, 'input[name="do"]'); // Signal for Nette/Application
+		$this->guy->see(null, 'button');
+	}
+
+	public function testValidation()
+	{
+		$this->guy->amOnPage('/article/');
+		$this->guy->seeResponseCodeIs(200);
+		$this->guy->fillField('#form_text', '');
+		$this->guy->click('button');
+		$this->guy->see('error', '.state');
+		$this->guy->see('This value should not be blank.');
+	}
+
+	public function testSuccess()
+	{
+		$this->guy->amOnPage('/article/');
+		$this->guy->seeResponseCodeIs(200);
+		$this->guy->fillField('#form_text', 'lorem ipsum');
+		$this->guy->click('button');
+		$this->guy->see('success', '.state');
+	}
+
+}
