cae1c337e6c6594cda39b8d72b0dc93924bdb24d
Jáchym Toušek <enumag@gmail.com>
2016-06-18 16:16:11 +0200

    Add render signal to FormComponent

--- src/Application/FormComponent.php
+++ src/Application/FormComponent.php
@@ -10,16 +10,21 @@
 
 namespace Arachne\Forms\Application;
 
+use Nette\Application\BadRequestException;
 use Nette\Application\Request;
 use Nette\Application\UI\BadSignalException;
 use Nette\Application\UI\ISignalReceiver;
 use Nette\Application\UI\Presenter;
 use Nette\ComponentModel\Component;
 use Nette\ComponentModel\IContainer;
+use Nette\Utils\Strings;
 use Symfony\Bridge\Twig\Form\TwigRendererInterface;
 use Symfony\Component\Form\FormErrorIterator;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\PropertyAccess\Exception\ExceptionInterface;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 use Twig_Environment;
 
 /**
@@ -71,15 +76,16 @@ class FormComponent extends Component implements ISignalReceiver
     protected $renderer;
 
     /**
-     * @param TwigRendererInterface $renderer
-     * @param Twig_Environment      $twig
-     * @param FormInterface         $form
+     * @var PropertyAccessorInterface
      */
-    public function __construct(TwigRendererInterface $renderer, Twig_Environment $twig, FormInterface $form)
+    protected $propertyAccessor;
+
+    public function __construct(TwigRendererInterface $renderer, Twig_Environment $twig, FormInterface $form, PropertyAccessorInterface $propertyAccessor = null)
     {
         $this->renderer = $renderer;
         $renderer->setEnvironment($twig); // Cannot be in DIC setup because of cyclic reference.
         $this->form = $form;
+        $this->propertyAccessor = $propertyAccessor ?: PropertyAccess::createPropertyAccessor();
     }
 
     /**
@@ -160,6 +166,8 @@ class FormComponent extends Component implements ISignalReceiver
             $this->processSubmit($form, $request);
         } elseif ($signal === 'validate') {
             $this->processValidate($form, $request);
+        } elseif ($signal === 'render') {
+            $this->processRender($form, $request);
         } else {
             throw new BadSignalException("Missing handler for signal '$signal' in ".get_class($this).'.');
         }
@@ -197,6 +205,7 @@ class FormComponent extends Component implements ISignalReceiver
         $this->walkErrors($form->getErrors(true, false), $view, function (FormView $view) use (&$errors) {
             $errors[$view->vars['id']] = $this->renderer->searchAndRenderBlock($view, 'errors_content');
         });
+
         $this->getPresenter()->sendJson((object) ['errors' => $errors]);
     }
 
@@ -211,5 +220,40 @@ class FormComponent extends Component implements ISignalReceiver
                 $new = false;
             }
         }
+    }
+
+    protected function processRender(FormInterface $form, Request $request)
+    {
+        $fields = $request->getPost($this->lookupPath('Nette\Application\UI\Presenter', true).'-fields');
+        $form->handleRequest($request);
+        if (!$fields || !$form->isSubmitted() || !$this->getPresenter()->isAjax()) {
+            return;
+        }
+
+        $view = $this->getView();
+        $widgets = [];
+        foreach ($fields as $field) {
+            // Validate the field identifier for security reasons. A dot in the identifier would be particularly dangerous.
+            if (!Strings::match($field, '~^(?:\[\w++\])++$~')) {
+                throw new BadRequestException("Field identifier '$field' containes unallowed characters.");
+            }
+
+            // Skip duplicates. The renderer would return an empty string on second try.
+            if (isset($widgets[$field])) {
+                continue;
+            }
+
+            // Wrap an exception from PropertyAccessor in a BadRequestException.
+            try {
+                $fieldView = $this->propertyAccessor->getValue($view, $field);
+            } catch (ExceptionInterface $e) {
+                throw new BadRequestException("FormView not found for field identifier '$field'.", 0, $e);
+            }
+
+            // Render the field widget.
+            $widgets[$field] = $this->renderer->searchAndRenderBlock($fieldView, 'widget');
+        }
+
+        $this->getPresenter()->sendJson((object) ['widgets' => $widgets]);
     }
 }
