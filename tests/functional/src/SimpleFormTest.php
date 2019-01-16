dcf75a9ab2db99c608782620adb0f43d2b6ae7c8
Jáchym Toušek <enumag@gmail.com>
2019-01-16 23:49:07 +0100

    Use phpstan 0.11

--- tests/functional/src/SimpleFormTest.php
+++ tests/functional/src/SimpleFormTest.php
@@ -21,12 +21,12 @@ class SimpleFormTest extends Unit
     {
         $this->tester->amOnPage('/article/');
         $this->tester->seeResponseCodeIs(200);
-        $this->tester->see(null, 'form');
-        $this->tester->see(null, 'input#form__token'); // CSRF protection
-        $this->tester->see(null, 'input[name="_do"]'); // Signal for Nette/Application
-        $this->tester->see(null, 'label.text-field-label');
-        $this->tester->see(null, 'input.text-field-widget');
-        $this->tester->see(null, 'button');
+        $this->tester->see('', 'form');
+        $this->tester->see('', 'input#form__token'); // CSRF protection
+        $this->tester->see('', 'input[name="_do"]'); // Signal for Nette/Application
+        $this->tester->see('', 'label.text-field-label');
+        $this->tester->see('', 'input.text-field-widget');
+        $this->tester->see('', 'button');
     }
 
     public function testPostMethodValidation(): void
@@ -35,7 +35,7 @@ class SimpleFormTest extends Unit
         $this->tester->seeResponseCodeIs(200);
         $this->tester->fillField('#form_text', '');
         $this->tester->click('button');
-        $this->tester->see(null, 'input[name="_do"]');
+        $this->tester->see('', 'input[name="_do"]');
         $this->tester->see('error', '.state');
         $this->tester->see('This value should not be blank.');
     }
@@ -46,7 +46,7 @@ class SimpleFormTest extends Unit
         $this->tester->seeResponseCodeIs(200);
         $this->tester->fillField('#form_text', 'lorem ipsum');
         $this->tester->click('button');
-        $this->tester->see(null, 'input[name="_do"]');
+        $this->tester->see('', 'input[name="_do"]');
         $this->tester->see('success', '.state');
     }
 
@@ -54,10 +54,10 @@ class SimpleFormTest extends Unit
     {
         $this->tester->amOnPage('/article/useget/1');
         $this->tester->seeResponseCodeIs(200);
-        $this->tester->see(null, 'form');
-        $this->tester->see(null, 'input#form__token'); // CSRF protection
-        $this->tester->see(null, 'input[name="do"]'); // Signal for Nette/Application
-        $this->tester->see(null, 'button');
+        $this->tester->see('', 'form');
+        $this->tester->see('', 'input#form__token'); // CSRF protection
+        $this->tester->see('', 'input[name="do"]'); // Signal for Nette/Application
+        $this->tester->see('', 'button');
     }
 
     public function testGetMethodValidation(): void
@@ -66,7 +66,7 @@ class SimpleFormTest extends Unit
         $this->tester->seeResponseCodeIs(200);
         $this->tester->fillField('#form_text', '');
         $this->tester->click('button');
-        $this->tester->see(null, 'input[name="do"]');
+        $this->tester->see('', 'input[name="do"]');
         $this->tester->see('error', '.state');
         $this->tester->see('This value should not be blank.');
     }
@@ -77,7 +77,7 @@ class SimpleFormTest extends Unit
         $this->tester->seeResponseCodeIs(200);
         $this->tester->fillField('#form_text', 'lorem ipsum');
         $this->tester->click('button');
-        $this->tester->see(null, 'input[name="do"]');
+        $this->tester->see('', 'input[name="do"]');
         $this->tester->see('success', '.state');
     }
 }
